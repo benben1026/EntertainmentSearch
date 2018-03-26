ff4043f1c6f881858044498d210c920c0dfe1b16
benben1026 <zhouwei1026@gmail.com>
2018-03-26 00:21:48 -0700

    Search API done.

--- mycode.js
+++ mycode.js
@@ -1,12 +1,34 @@
+const DOMAIN = "http://localhost:8888"
+
 function init(){
+	$.ajax({
+		url: "http://ip-api.com/json",
+		dataType: "json",
+		success: function(data){
+			client_lat = data['lat'];
+			client_lng = data['lon'];
+		},
+		error: function(){
+			console.log("Cannot get client coordinate")
+		}
+	})
+
+
 	$("#search-form").submit(function(event){
 		event.preventDefault();
+		var url = DOMAIN + "/search?keyword=" + $('#keyword').val() 
+				+ "&radius=" + parseInt($('#distance').val()) * 1609.34 
+				+ "&from=" + ($('[name="from"]')[0].checked ? "here" : "customized")
+				+ "&category=" + $('#category').val()
+				+ "&location=" + $('#location').val()
+				+ "&lat=" + client_lat
+				+ "&lng=" + client_lng;
+		console.log(url)
 		$.ajax({
-			url: "http://cs-server.usc.edu:29709/index.php?type=SUBMIT&keyword=usc&distance=10&from=here&category=default&location=&lat=34.0035&lng=-118.2873",
-			type: "get",
-			data: "json",
-			success: function(data){alert("success")},
-			errer: function(){alert("error")}
+			url: url,
+			type: 'get',
+			dataType: 'json',
+			success: function(data){console.log(data)}
 		})
 	});
 }
