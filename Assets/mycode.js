41f4863ff3bc23d12ad41f93604ceb34bd6dc283
benben1026 <zhouwei1026@gmail.com>
2018-03-31 14:33:13 -0700

    Code reorgnaize

---
+++ Assets/mycode.js
@@ -0,0 +1,237 @@
+const DOMAIN = "http://localhost:8888"
+
+function set_place_detail(place, status){
+	// console.log(status)
+	// console.log(place)
+	if (status == google.maps.places.PlacesServiceStatus.OK){
+		set_info(place);
+		set_map(place);
+		set_photo(place);
+		set_google_reviews(place);
+		set_yelp_reviews(place);
+	}
+}
+
+function set_info(data){
+	$('#info').html('')
+	$('#info').append('<tr><td>Address</td><td>' + data['adr_address'] + '</td></tr>');
+	if (data['international_phone_number'])
+		$('#info').append('<tr><td>Phone Number</td><td>' + data['international_phone_number'] + '</td></tr>');
+	if (data['price_level'])
+		$('#info').append('<tr><td>Price Level</td><td>' + data['price_level'] + '</td></tr>');
+	if (data['rating'])
+		$('#info').append('<tr><td>Rating</td><td>' + data['rating'] + '</td></tr>');
+	if (data['url'])
+		$('#info').append('<tr><td>Google Page</td><td><a target="__blank" href="' + data['url'] + '">' + data['url'] + '</a></td></tr>');
+	if (data['website'])
+		$('#info').append('<tr><td>Website</td><td><a target="__blank" href="' + data['website'] + '">' + data['website'] + '</a></td></tr>');
+	if (data['opening_hours']){
+		var innerhtml = "";
+		var currentdate = new Date(Date.now() + (new Date().getTimezoneOffset() * 60000) - parseInt(data['utc_offset']) * 60000) ;
+		var day = currentdate.getDay();
+		if (data['opening_hours']['open_now']){
+			innerhtml += "Open " + data['opening_hours']['weekday_text'][day];
+		} else {
+			innerhtml += "Closed"
+		}
+		innerhtml += "&nbsp;&nbsp;<a>Daily open hours</a>"
+		$('#info').append('<tr><td>Hours</td><td>' + innerhtml + '</td></tr>');
+	}
+
+}
+
+function set_photo(data){
+	if (!data.photos && data.photos.length > 0){
+		return;
+	}
+	$('.photo-container').html('');
+	var photo_container_index = 0;
+	for(var i in data.photos){
+		if (photo_container_index >= 4){
+			photo_container_index = 0;
+		}
+		var maxwidth = data.photos[i]['width'];
+		var thumbnail_url = data.photos[i].getUrl({maxWidth: 200});
+		var photo_url = data.photos[i].getUrl({maxWidth: maxwidth});
+		$('div.photo-container:eq(' + photo_container_index + ')').append('<a target="__blank" href="' + photo_url + '"><img class="thumbnail" style="width:100%" src="' + thumbnail_url + '"></a>')
+		photo_container_index += 1;
+	}
+}
+
+function set_map(data){
+	$('#map-to').val(data.name);
+	$('#map-from').val("Your location");
+	destLat = data['geometry']['location']['lat']();
+	destLng = data['geometry']['location']['lng']();
+	var name = data['name']
+	var latLng = new google.maps.LatLng({lat: parseFloat(destLat), lng:parseFloat(destLng)});
+	marker = new google.maps.Marker({
+		position: latLng,
+		map: map,
+		title: name
+	});
+	map.setCenter(latLng);
+}
+
+function getDirection(){
+	if ($('#map-from').val().trim().toLowerCase() == "your location"){
+		fromLocationLat = client_lat;
+		fromLocationLng = client_lng;
+		calculateAndDisplayRoute();
+		return;
+	}
+	$.ajax({
+		url: DOMAIN + "/geocode?location=" + encodeURIComponent($('#map-from').val()),
+		dataType: 'json',
+		success: function(data){
+			fromLocationLat = data['lat'];
+			fromLocationLng = data['lng'];
+			calculateAndDisplayRoute();
+		},
+		error: function(){
+			console.log('error');
+		}
+	})
+}
+
+function calculateAndDisplayRoute(){
+	marker.setMap(null);
+	directionsService.route({
+		origin: {lat: parseFloat(fromLocationLat), lng: parseFloat(fromLocationLng)}, 
+		destination: {lat: parseFloat(destLat), lng: parseFloat(destLng)},
+		travelMode: google.maps.TravelMode[$('#travel-mode').val()]
+	}, function(response, status){
+		if (status == 'OK') {
+			directionsDisplay.setDirections(response);
+		} else {
+			window.alert('Directions request failed due to ' + status);
+		}
+	})
+}
+
+function set_google_reviews(data){
+	for (var i in data.reviews){
+		var profile = '<img src="' + data.reviews[i].profile_photo_url + '">';
+		var name = '<span>' + data.reviews[i].author_name + '</span>';
+		var rating = data.reviews[i].rating;
+		var date = new Date(parseInt(data.reviews[i].time));
+		var comment = data.reviews[i].text;
+		$('#google-reviews').append('<li class="list-group-item">' + profile + name + rating + date + comment + '</li>')
+	}
+}
+
+function set_yelp_reviews(data){
+	for (var i in data.address_components){
+		if ($.inArray('country', data.address_components[i].types) > -1){
+			var country = data.address_components[i].short_name;
+		} else if($.inArray('administrative_area_level_1', data.address_components[i].types) > -1){
+			var state = data.address_components[i].short_name;
+		} else if($.inArray('administrative_area_level_2', data.address_components[i].types) > -1){
+			var city = data.address_components[i].short_name;
+		} else if($.inArray('postal_code', data.address_components[i].types) > -1){
+			var postal = data.address_components[i].short_name;
+		} else if($.inArray('street_number', data.address_components[i].types) > -1){
+			var street_number = data.address_components[i].short_name
+		} else if($.inArray('route', data.address_components[i].types) > -1){
+			var route = data.address_components[i].short_name
+		}
+	}
+	var param = {
+		name: encodeURIComponent(data['name']),
+		city: encodeURIComponent(city),
+		state: state,
+		country: country,
+		address: encodeURIComponent(street_number + " " + route),
+		postal: postal
+	}
+	console.log(DOMAIN + "/getyelpreviews?" + $.param(param))
+	$.ajax({
+		url: DOMAIN + "/getyelpreviews?" + $.param(param),
+		dataType: 'json',
+		success: function(data){
+			if (data['status'] != 'OK'){
+				console.log(data['status']);
+				return;
+			}
+			data = data['data'];
+			for (var i in data.reviews){
+				var profile = '<img src="' + data.reviews[i].user.image_url + '">';
+				var name = '<span>' + data.reviews[i].user.name + '</span>';
+				var rating = data.reviews[i].rating;
+				var date = data.reviews[i].time_created;
+				var comment = data.reviews[i].text;
+				$('#yelp-reviews').append('<li class="list-group-item">' + profile + name + rating + date + comment + '</li>')
+			}
+		},
+		error: function(){
+			console.log('error');
+		}
+	})
+}
+
+function init(){
+	$.ajax({
+		url: "http://ip-api.com/json",
+		dataType: "json",
+		success: function(data){
+			client_lat = data['lat'];
+			client_lng = data['lon'];
+		},
+		error: function(){
+			console.log("Cannot get client coordinate")
+		}
+	})
+
+
+	$("#search-form").submit(function(event){
+		event.preventDefault();
+		var url = DOMAIN + "/search?keyword=" + $('#keyword').val() 
+				+ "&radius=" + parseInt($('#distance').val()) * 1609.34 
+				+ "&from=" + ($('[name="from"]')[0].checked ? "here" : "customized")
+				+ "&category=" + $('#category').val()
+				+ "&location=" + $('#location').val()
+				+ "&lat=" + client_lat
+				+ "&lng=" + client_lng;
+		console.log(url)
+		$.ajax({
+			url: url,
+			type: 'get',
+			dataType: 'json',
+			success: function(data){
+				if (data['status'] == 'OK'){
+					console.log(data);
+					for (var i in data['data']){
+						$('#search-result tr:last').after('<tr><td>' + (parseInt(i) + 1) + '</td>' 
+							+ '<td><img src=\"' + data['data'][i]['icon'] + '\" /></td>' 
+							+ '<td>' + data['data'][i]['name'] + '</td>' 
+							+ '<td>' + data['data'][i]['address'] + '</td>' 
+							+ '<td><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span></button></td>' 
+							+ '<td><button type="button" onclick="javascript:service.getDetails({placeId: \'' + data['data'][i]['id'] + '\'}, set_place_detail);" class="btn btn-default"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button></td></tr>')
+					}
+				}
+			},
+			error: function(){
+				console.log('error');
+			}
+		})
+	});
+
+	$("#get-direction-btn").click(function(event){
+		event.preventDefault();
+		getDirection();
+	})
+	service = new google.maps.places.PlacesService(map);
+}
+
+function initMap(){
+	$('#map').html('');
+	directionsDisplay = new google.maps.DirectionsRenderer;
+	directionsService = new google.maps.DirectionsService;
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 14,
+		center: {lat: 37.77, lng: -122.447}
+	});
+	directionsDisplay.setMap(map);
+}
+
+$(window).on("load", init);
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const request = require('request')
 const cors = require('cors')
 const url = require('url')
+const path = require('path')
+const querystring = require('querystring');
 
 const app = express()
 const port = 8888
@@ -11,7 +13,7 @@ app.use(cors())
 function geocode(callback, location){
     request('https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=' + process.env.API_KEY, function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            result = JSON.parse(body);
+            var result = JSON.parse(body);
             result = {"lat": result['results'][0]['geometry']['location']['lat'], 
             	"lng": result['results'][0]['geometry']['location']['lng']}
             return callback(null, result);
@@ -36,7 +38,7 @@ function nearbysearch(callback, lat, lng, radius, type, keyword){
 		+ lat + "," + lng + "&radius=" + radius + "&type=" + type + "&keyword=" + keyword + "&key=" + process.env.API_KEY;
 	request(request_url, function(error, response, body){
 		if (!error && response.statusCode == 200) {
-            result = JSON.parse(body);
+            var result = JSON.parse(body);
             return callback(null, result);
         } else {            
             return callback(error, null);;
@@ -90,9 +92,123 @@ app.get('/search', function(req, res){
 	}
 })
 
+function placedetail(callback, place_id){
+	var request_url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + place_id + "&key=" + process.env.API_KEY;
+	request(request_url, function(error, response, body){
+		if (!error && response.statusCode == 200){
+			var result = JSON.parse(body);
+			return callback(null, result);
+		} else {
+			return callback(error, null);
+		}
+	})
+}
+
+app.get('/placedetail', function(req, res){
+	var qdata = url.parse(req.url, true).query;
+	placedetail(function(err, data){
+		if (err){
+			res.send({'status': 'ERROR', 'msg': err})
+			return;
+		}
+		var output = {'status':'OK', 'address': data['result']['adr_address'], 
+			'phone_number': data['result']['international_phone_number'],
+			'price_level': data['result']['price_level'],
+			'rating': data['result']['rating'],
+			'website': data['result']['website'],
+			'google_page': data['result']['url'],
+			'hour': data['result']['opening_hours'],
+			'utc_offset': data['result']['utc_offset'],
+			'reviews': data['result']['reviews'],
+			'location': data['result']['geometry']['location'],
+			'name': data['result']['name']
+		}
+		res.send(JSON.stringify(output));
+	}, qdata.id)
+})
+
+function yelpGetReviews(callback, id){
+	request({
+		url: "https://api.yelp.com/v3/businesses/" + id + "/reviews",
+		headers: {
+			"Authorization": "Bearer " + process.env.YELP_API_KEY
+		}},
+		function(error, response, body){
+			if (!error && response.statusCode == 200){
+				var result = JSON.parse(body);
+				return callback(null, result);
+			} else {
+				return callback(error, null);
+			}
+		}
+	);
+}
+
+function yelpGetId(callback, input_para){
+	//console.log("https://api.yelp.com/v3/businesses/matches/best?" + querystring.stringify(input_para))
+	request({
+		url: "https://api.yelp.com/v3/businesses/matches/best?" + querystring.stringify(input_para),
+		headers: {
+			"Authorization": "Bearer " + process.env.YELP_API_KEY
+		}},
+		function(error, response, body){
+			if (!error && response.statusCode == 200){
+				var result = JSON.parse(body);
+				console.log(result);
+				return callback(null, result);
+			} else {
+				return callback(error, null);
+			}
+		}
+	)
+}
+
+app.get('/getyelpreviews', function(req, res){
+	if (!process.env.YELP_API_KEY){
+		res.send(JSON.stringify({status: 'ERROR', msg:'API_KEY_NOT_SET'}));
+		return;
+	}
+	var qdata = url.parse(req.url, true).query;
+	var param = {
+		name: querystring.unescape(qdata.name),
+		city: querystring.unescape(qdata.city),
+		state: querystring.unescape(qdata.state),
+		country: querystring.unescape(qdata.country),
+		address1: querystring.unescape(qdata.address),
+		postal_code: querystring.unescape(qdata.postal)
+	}
+	yelpGetId(function(err, data){
+		if (err){
+			res.send({status: err})
+			return;
+		}
+		//console.log(data);
+		if (data['businesses'].length == 0){
+			res.send({status: 'NO_BUSINESE_FOUND'})
+			return;
+		}
+		yelpGetReviews(function(err, data){
+			if (err){
+				res.send({status: err})
+				return;
+			}
+			res.send(JSON.stringify({status: 'OK', data: data}));
+		}, data['businesses'][0]['id'])
+	}, param);
+})
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.use(express.static(__dirname + '/Assets/'));
+
 
 if (!process.env.API_KEY){
 	console.log("API KEY not set");
 }else{
+	if (!process.env.YELP_API_KEY){
+		console.log("YELP API KEY not set");
+	}
 	app.listen(port, () => console.log('Server listening on port ' + port))
 }
